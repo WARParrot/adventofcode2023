d8f79a324a82a44df15f9be32e7d93c74f7e2053
WARParrot <103835802+WARParrot@users.noreply.github.com>
2023-12-02 14:41:37 +0700

    added day2

---
+++ day2/task2.py
@@ -0,0 +1,23 @@
+IDs = int()
+
+with open("input2.txt") as f:
+    file = f.read().splitlines()
+
+for i in file:
+    ID = int(i.split(':')[0].split()[1])
+    i = i.split(':')[1]
+    red, green, blue = 0, 0, 0
+
+    for e in i.split('; '):
+        for k in e.split(', '):
+            h = k.split()
+            if h[1] == 'red' and int(h[0]) > red:
+                red = int(h[0])
+            elif h[1] == 'green' and int(h[0]) > green:
+                green = int(h[0])
+            elif h[1] == 'blue' and int(h[0]) > blue:
+                blue = int(h[0])
+    IDs += red * green * blue
+
+
+print(IDs)
